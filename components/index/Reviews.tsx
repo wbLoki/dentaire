62860fceda8fc8b1e8177ba1ccbd3d5f5a140388
Wail Bouymaj <hun73r@icloud.com>
2023-02-27 17:56:12 +0100

    remove dependacy

---
+++ components/index/Reviews.tsx
@@ -0,0 +1,16 @@
+import React from 'react'
+
+const testim = [
+    {name: 'Reda Nahab', text: 'Je recommande vivement Dr Majd Zade, une bonne personne, professionel, il a de bonnes qualités et il maitrise bien son métier.', rating: 5},
+    {name: 'Reda Nahab', text: 'Je recommande vivement Dr Majd Zade, une bonne personne, professionel, il a de bonnes qualités et il maitrise bien son métier.', rating: 5},
+    {name: 'Reda Nahab', text: 'Je recommande vivement Dr Majd Zade, une bonne personne, professionel, il a de bonnes qualités et il maitrise bien son métier.', rating: 5},
+];
+type Props = {}
+
+function Reviews({}: Props) {
+  return (
+    <div></div>
+  )
+}
+
+export default Reviews
