a19ebb1ab3f427525a71cb74304dc9882c880ef1
Wail Bouymaj <hun73r@icloud.com>
2023-04-03 00:40:59 +0000

    update keywords for SEO

--- pages/index.tsx
+++ pages/index.tsx
@@ -12,6 +12,7 @@ import Location from '@/components/index/Location'
 import Footer from '@/components/Footer'
 
 const inter = Inter({ subsets: ['latin'] })
+const keywords = "cabinet, dentaire, tanger, dentiste, dentist, implant dentaire, طبيب أسنان طنجة, طبيب أسنان بوخالف, احسن طبيب أسنان طنجة, أرقام هواتف أطباء الأسنان طنجة";
 
 export default function Home() {
   return (
@@ -19,7 +20,7 @@ export default function Home() {
       <Head>
         <title>Cabinet Dentaire Ennasr</title>
         <meta name="description" content="Cabinet Dentaire Ennasr de Tanger" />
-        <meta name="keywords" content="cabinet, dentaire, tanger, dentiste, dentist"/>
+        <meta name="keywords" content={keywords}/>
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link key="icon" rel="icon" href="/logo.jpg" />
       </Head>
