e5165f31f338f0ef133dd2b6da8727c371e6f249
Wail Bouymaj <hun73r@icloud.com>
2023-04-03 20:54:11 +0000

    update Head component

--- pages/index.tsx
+++ pages/index.tsx
@@ -12,15 +12,13 @@ import Location from '@/components/index/Location'
 import Footer from '@/components/Footer'
 
 const inter = Inter({ subsets: ['latin'] })
-const keywords = "cabinet, dentaire, tanger, dentiste, dentist, implant dentaire, طبيب أسنان طنجة, طبيب أسنان بوخالف, احسن طبيب أسنان طنجة, أرقام هواتف أطباء الأسنان طنجة";
 
 export default function Home() {
   return (
     <>
       <Head>
         <title>Cabinet Dentaire Ennasr</title>
-        <meta name="description" content="Cabinet Dentaire Ennasr de Tanger" />
-        <meta name="keywords" content={keywords}/>
+        <meta name="description" content="Cabinet Dentaire Ennasr de Tanger. Prenez un rendez-vous avec l'un des meilleurs médecins dès maintenant." />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link key="icon" rel="icon" href="/logo.jpg" />
       </Head>
