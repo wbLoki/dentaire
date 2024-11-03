a60c07c2e451837268de6967411d99af6a7f801c
WailBouymaj-Kenzup <wail.bouymaj@kenzup.com>
2024-11-03 17:09:33 +0100

    allow images from google api

--- next.config.js
+++ next.config.js
@@ -3,6 +3,10 @@ const createNextIntlPlugin = require('next-intl/plugin');
 const withNextIntl = createNextIntlPlugin();
 
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  images: {
+    domains: ['lh3.googleusercontent.com'], // Allow images from Googleusercontent
+  },
+};
 
 module.exports = withNextIntl(nextConfig);
